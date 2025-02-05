be7db4bec7c5f53d12a6ed9b4a1b3679c82b146d
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2025-02-05 20:01:58 +0200

    convertor styles finished

--- src/app/pages/convert/index.tsx
+++ src/app/pages/convert/index.tsx
@@ -51,41 +51,62 @@ export default function index() {
   }
 
   return (
-    <div className='p-4'>
-      <h1 className='text-2xl font-bold mb-4'>Currency Converter</h1>
-
-      <div className='mb-4'>
-        <input
-          type='number'
-          value={amount}
-          onChange={(e) => setAmount(Number(e.target.value))}
-          className='p-2 border rounded w-full'
-        />
+    <section className='px-4 xl:px-[150px] mt-[60px] w-full'>
+      <div className='m-auto flex max-w-[1042px] flex-col'>
+        <h1 className='text-center text-[56px] font-semibold tracking-[-1px] xl:text-[68px] mb-8 xl:mb-[60px] '>
+          Currency Converter
+        </h1>
+        <form className='mr-3 mt-4 flex flex-col  gap-6 rounded-[24px] border-[2px] border-[#99A2FF] bg-white p-6 shadow-[12px_-12px_0_0_#99A2FF] xl:mr-0  xl:gap-4 xl:px-[30px] xl:py-5 xl:shadow-[16px_-16px_0_0_#99A2FF]'>
+          <div className='relative grid grid-cols-1 grid-rows-1 gap-2 md:grid-cols-[33%_1fr] md:grid-rows-[none]'>
+            <div className='h-[84px] rounded-lg border border-solid border-gray-250 bg-white px-4 py-2 text-2xl font-semibold hover:bg-gray-150 '>
+              <label className=' block text-sm font-medium text-gray-400'>
+                Amount
+              </label>
+              <input
+                type='number'
+                value={amount}
+                onChange={(e) => {
+                  const value = Number(e.target.value)
+                  if (value > 0) {
+                    setAmount(value)
+                  }
+                }}
+                className='w-full h-10 py-2 px-4 mt-[3px] rounded-[10px] placeholder:text-[16px] leading-6 outline-none text-darkBlack'
+                min='1'
+              />
+            </div>
+            <div className='flex flex-col justify-evenly gap-2 md:flex-row'>
+              <CurrencySelector
+                value={from}
+                onChange={setFrom}
+                currencies={Object.keys(rates)}
+              />
+              <CurrencySelector
+                value={to}
+                onChange={setTo}
+                currencies={Object.keys(rates)}
+              />
+            </div>
+          </div>
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              handleConvert()
+            }}
+            disabled={loading}
+            className='flex min-h-11 min-w-16 cursor-pointer items-center justify-center self-end rounded-2xl border border-lightPurple font-medium shadow-[0_4px_16px_0_#1A23721A] w-full gap-0 bg-purpleMain p-4 text-[18px] text-white hover:bg-purpleMain hover:shadow-[0_4px_16px_0_#6776FC4D] xl:w-auto flex-row-reverse'
+          >
+            {loading ? 'Converting...' : 'Convert'}
+          </button>
+          {error && <p className='text-red-500 mt-2'>{error}</p>}
+          {result !== null && (
+            <p className='mt-2 text-lg text-[18px] md:text-[32px] leading-6  text-darkBlack'>
+              <span className='font-semibold'>Current value:</span>
+              {resultMessage.current}
+            </p>
+          )}
+        </form>
       </div>
-
-      <CurrencySelector
-        value={from}
-        onChange={setFrom}
-        currencies={Object.keys(rates)}
-      />
-      <CurrencySelector
-        value={to}
-        onChange={setTo}
-        currencies={Object.keys(rates)}
-      />
-
-      <button
-        onClick={handleConvert}
-        disabled={loading}
-        className='bg-blue-500 text-white px-4 py-2 rounded mt-4'
-      >
-        {loading ? 'Converting...' : 'Convert'}
-      </button>
-
-      {error && <p className='text-red-500 mt-2'>{error}</p>}
-      {result !== null && (
-        <p className='mt-4 text-lg'>{resultMessage.current}</p>
-      )}
-    </div>
+    </section>
   )
 }
